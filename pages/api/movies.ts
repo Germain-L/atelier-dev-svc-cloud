8303237fb63b4aff35ecae16a25cb27abd6e6d4a
Germain-L <germain.leignel@gmail.com>
2024-02-26 14:50:22 +0100

    tools

---
+++ pages/api/movies.ts
@@ -0,0 +1,20 @@
+import {NextApiRequest, NextApiResponse} from 'next';
+import clientPromise from "../../lib/mongodb";
+
+type ResponseData = {
+    status: number;
+    data: any[];
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>) {
+    const client = await clientPromise;
+    const db = client.db("sample_mflix");
+    const movies = await db.collection("movies")
+        .find({})
+        .limit(10)
+        .toArray();
+
+    res.json({status: 200, data: movies})
+}
