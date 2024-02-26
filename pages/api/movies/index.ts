51095c01938ffb590c00fb43ad83e700512915b5
Germain-L <germain.leignel@gmail.com>
2024-02-26 14:58:04 +0100

    endpoint files

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,5 +1,5 @@
 import {NextApiRequest, NextApiResponse} from 'next';
-import clientPromise from "../../lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
 
 type ResponseData = {
     status: number;
